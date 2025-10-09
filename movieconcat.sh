780870ed2a83856b4a38b9a442578e44e8266d56
spiner321 <luckyboy3214@naver.com>
2025-10-09 22:28:42 +0900

    영상 정밀 컷팅

--- movieconcat.sh
+++ movieconcat.sh
@@ -1,3 +1,3 @@
-cd "/mnt/c/Users/lucky/Videos/방송/동키콩 바난자"
+cd "/mnt/c/Users/lucky/Videos/방송/하늘의 궤적"
 
-ffmpeg -f concat -safe 0 -i list.txt -c copy "바난자 수련 합쳐진영상.mkv"
+ffmpeg -f concat -safe 0 -i /mnt/d/workspace/movieclip/list.txt -c copy "7화.mp4"
--- moviecut.sh
+++ moviecut.sh
@@ -1,12 +1,20 @@
-cd "/mnt/c/Users/lucky/Videos/방송/동키콩 바난자"
+cd "/mnt/c/Users/lucky/Videos/방송/"
 
-# # part1: 00:55:45 ~ 00:56:55 (1분 10초 = 00:01:10)
-# echo y | ffmpeg -ss 00:55:45 -i aaa.mkv -t 00:01:10 -c copy part1.mkv
+## 영상 단순 컷팅 (1단계) ##
+# echo y | ffmpeg -i "2~3.mkv" -ss 00:00:00 -t 07:22:33 -c copy "2화.mkv"
+# echo y | ffmpeg -i "2~3.mkv" -ss 07:22:45 -c copy "3화.mkv"
 
-# # part2: 00:57:55 ~ 00:58:56 (1분 1초 = 00:01:01)
-# echo y | ffmpeg -ss 00:57:55 -i aaa.mkv -t 00:01:01 -c copy part2.mkv
+# 2화 (0:00:00 ~ 7:22:33)
+# echo y | ffmpeg -ss 00:00:00 -i "2~3.mkv" -t 07:22:33 -c copy "2화.mkv"
 
-# # part3: 01:13:38 ~ 02:07:43 (54분 5초 = 00:54:05)
-# echo y | ffmpeg -ss 01:13:38 -i aaa.mkv -t 00:54:05 -c copy part3.mkv
+# 3화 (7:22:45 ~ 끝까지)
+# echo y | ffmpeg -ss 06:35 -i "2025-10-09 21-50-59.mp4" -c copy "셀레기오스.mp4"
 
-echo y | ffmpeg -i "바난자 수련 1.mkv" -t 2:09:10 -c copy "바난자 수련 1 cut.mkv"
+
+
+## 영상 정밀 컷팅 (2단계) ##
+# 1단계: 근처까지만 빠르게 잘라 임시 파일 생성
+# echo y | ffmpeg -ss 00:06:00 -i "2025-10-09 21-50-59.mp4" -c copy tmp.mp4
+
+# 2단계: 정확히 원하는 시점부터 다시 정밀 컷
+echo y | ffmpeg -i tmp.mp4 -ss 00:00:37 -c copy "셀레기오스.mp4"
