6731a8b823ca3198a05b67cdbe8d12c6d5fb6bd8
spiner321 <luckyboy3214@naver.com>
2026-01-11 02:02:25 +0900

    update

--- movieconcat.sh
+++ movieconcat.sh
@@ -1,3 +1,3 @@
-cd "/mnt/c/Users/lucky/Videos/방송/하늘의 궤적"
+cd "/mnt/c/Users/lucky/Videos/방송/블러드본"
 
-ffmpeg -f concat -safe 0 -i /mnt/d/workspace/movieclip/list.txt -c copy "7화.mp4"
+ffmpeg -f concat -safe 0 -i /mnt/d/workspace/movieclip/list.txt -c copy "1화_수정.mp4"
--- moviecut.sh
+++ moviecut.sh
@@ -1,20 +1,9 @@
-cd "/mnt/c/Users/lucky/Videos/방송/"
+cd "/mnt/c/Users/lucky/Videos/방송/하이파이 러쉬"
 
-## 영상 단순 컷팅 (1단계) ##
-# echo y | ffmpeg -i "2~3.mkv" -ss 00:00:00 -t 07:22:33 -c copy "2화.mkv"
-# echo y | ffmpeg -i "2~3.mkv" -ss 07:22:45 -c copy "3화.mkv"
+# # 1화 앞부분: 00:00:00 ~ 00:02:01
+# echo y | ffmpeg -i "1화.mp4" -to 00:02:01 -c copy "1화_part1.mp4"
 
-# 2화 (0:00:00 ~ 7:22:33)
-# echo y | ffmpeg -ss 00:00:00 -i "2~3.mkv" -t 07:22:33 -c copy "2화.mkv"
+# # 1화 뒷부분: 00:06:18 ~ 끝
+# echo y | ffmpeg -ss 00:06:18 -i "1화.mp4" -c copy "1화_part2.mp4"
 
-# 3화 (7:22:45 ~ 끝까지)
-# echo y | ffmpeg -ss 06:35 -i "2025-10-09 21-50-59.mp4" -c copy "셀레기오스.mp4"
-
-
-
-## 영상 정밀 컷팅 (2단계) ##
-# 1단계: 근처까지만 빠르게 잘라 임시 파일 생성
-# echo y | ffmpeg -ss 00:06:00 -i "2025-10-09 21-50-59.mp4" -c copy tmp.mp4
-
-# 2단계: 정확히 원하는 시점부터 다시 정밀 컷
-echo y | ffmpeg -i tmp.mp4 -ss 00:00:37 -c copy "셀레기오스.mp4"
+echo y | ffmpeg -i "오프닝.mp4" -to 00:04:17 -c copy "오프닝_part1.mp4"
