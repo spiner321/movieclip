6c30153e0ec826f25fd86e634df4226208ca9c30
spiner321 <luckyboy3214@naver.com>
2026-01-18 03:09:48 +0900

    moviecut 방식 변경

--- movieconcat.sh
+++ movieconcat.sh
@@ -1,3 +1,3 @@
-cd "/mnt/c/Users/lucky/Videos/방송/블러드본"
+cd "/mnt/c/Users/lucky/Videos/방송/하이파이 러쉬"
 
-ffmpeg -f concat -safe 0 -i /mnt/d/workspace/movieclip/list.txt -c copy "1화_수정.mp4"
+ffmpeg -f concat -safe 0 -i /mnt/d/workspace/movieclip/list.txt -c copy "3화.mp4"
--- moviecut.sh
+++ moviecut.sh
@@ -1,9 +1,7 @@
 cd "/mnt/c/Users/lucky/Videos/방송/하이파이 러쉬"
 
-# # 1화 앞부분: 00:00:00 ~ 00:02:01
-# echo y | ffmpeg -i "1화.mp4" -to 00:02:01 -c copy "1화_part1.mp4"
+# 1화 앞부분: 00:00:00 ~ 00:02:01
+echo y | ffmpeg -i "3-1.mp4" -to 00:16:00 -c copy "3-1_part1.mp4"
 
-# # 1화 뒷부분: 00:06:18 ~ 끝
-# echo y | ffmpeg -ss 00:06:18 -i "1화.mp4" -c copy "1화_part2.mp4"
-
-echo y | ffmpeg -i "오프닝.mp4" -to 00:04:17 -c copy "오프닝_part1.mp4"
+# 1화 뒷부분: 00:06:18 ~ 끝
+echo y | ffmpeg  -i "1화.mp4" -ss 00:06:18-c copy "1화_part2.mp4"
